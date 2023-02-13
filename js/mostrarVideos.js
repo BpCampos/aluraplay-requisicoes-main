ce810d6beebd24ee7d587013bb5a81bbe6c47ea8
Bruno Campos <campos.bruno98@gmail.com>
2023-02-13 18:38:07 -0300

    initial commit

---
+++ js/mostrarVideos.js
@@ -0,0 +1,33 @@
+import { conectaApi } from "./conectaApi.js";
+const lista = document.querySelector("[data-lista]");
+
+export default function constroiCard(titulo, descricao, url, imagem) {
+    const video = document.createElement('li')
+    video.className = 'videos__item';
+    video.innerHTML = `
+        <iframe width="100%" height="72%" src=${url}
+        title=${titulo} frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+    <div class="descricao-video">
+        <img src="${imagem}" alt="logo canal alura">
+        <h3>${titulo}</h3>
+        <p>${descricao}</p>
+    </div>
+    `
+
+    return video;
+}
+
+async function listaVideos() {
+    try {
+        const listaApi = await conectaApi.listaVideos();
+        listaApi.forEach(element => lista.appendChild(constroiCard(element.titulo, element.descricao, element.url, element.imagem)));
+    } catch (erro) {
+        console.log(erro)
+        lista.innerHTML = `<h2 class="mensagem__titulo" style="font-size: 2rem; margin: 0 auto">Não foi possivel carregar a lista de videos</h2>`
+    }
+}
+
+listaVideos();
+
