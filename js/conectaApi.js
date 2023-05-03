8aee8c17176f76ad0247c039d9dbec8e8ad0820e
Bruno Campos <campos.bruno98@gmail.com>
2023-05-03 15:17:57 -0300

    hosting the json file in a my json server

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,11 +1,11 @@
 async function listaVideos() {
-    const conexao = await fetch('http://localhost:3000/videos')
+    const conexao = await fetch('https://my-json-server.typicode.com/BpCampos/aluraplay-api/videos')
     const conexaoConvertida = await conexao.json()
     return conexaoConvertida
 }
 
 async function criaVideo(titulo, descricao, url, imagem) {
-    const conexao = await fetch('http://localhost:3000/video', {
+    const conexao = await fetch('https://my-json-server.typicode.com/BpCampos/aluraplay-api/videos', {
         method: "POST",
         headers: {
             "Content-type": "application/json"
@@ -27,7 +27,7 @@ async function criaVideo(titulo, descricao, url, imagem) {
 
 
 async function buscaVideo(termoDeBusca) {
-    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`)
+    const conexao = await fetch(`https://my-json-server.typicode.com/BpCampos/aluraplay-api/videos?q=${termoDeBusca}`)
     const conexaoConvertida = conexao.json()
 
     return conexaoConvertida;
